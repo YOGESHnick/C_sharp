8c9efcf2ca0c82d6789573cc84ddee7507ffaf36
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2025-04-03 19:51:28 +0530

    Complete task-2

---
+++ task2/OOPDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+// Step 1: Create a Person class
+class Person 
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    // Constructor
+    public Person(string name, int age) 
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Method to introduce 
+    public void Introduce() 
+    {
+        Console.WriteLine($"Hi, I'm {Name} and I'm {Age} years old.");
+    }
+}
+
+
+class Program 
+{
+    static void Main() 
+    {
+
+        Person p1 = new Person("Alice", 25);
+        Person p2 = new Person("Bob", 30);
+
+        p1.Introduce();
+        p2.Introduce();
+    }
+}
